76948168e581e39a790b0e9c68c4da49741251d9
Antonin Lemoine <antonin.lemoine@altitudeinfra.fr>
2021-06-11 19:06:07 +0200

    add create database

--- sql/BD_EducaOps_v2.sql
+++ sql/BD_EducaOps_v2.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Base de données : `BD_EducaOps`
 --
 
+CREATE DATABASE IF NOT EXISTS `BD_EducaOps` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `BD_EducaOps`;
+
 DELIMITER $$
 --
 -- Procédures
